df83e12252f1964a4800cd8f342c3eac3ee9e612
Michael Savage <mike@mikejsavage.co.uk>
2024-05-24 10:56:01 +0300

    Add DocSetUtil as a submodule and update to GLFW 3.4

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ glfw-dash-docset
 ================
 
 A Dash docset for [GLFW](http://www.glfw.org).
+
+It only works on macOS and you run it like `nix-shell --packages doxygen --command ./build.sh`.
--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
-#!/bin/bash
-git submodule init
-git submodule update
+#! /usr/bin/env bash
+
+set -eo
+
+PATH="$(pwd)/DocSetUtil/bin:$PATH"
+
+git submodule update --init --force
+
+cd DocSetUtil
+	make "prefix=$(pwd)" install || true
+cd ..
+
+cd glfw
+	sed -i"" -e "s/1\\.9\\.8/1\\.9\\.7/" docs/CMakeLists.txt
+cd ..
+
+rm -rf build
 mkdir -p build && cd build && cmake ../glfw
+
 cd docs && patch Doxyfile < ../../Doxyfile.patch && make
+sed -i"" -e "s/\$(XCODE_INSTALL)\\/usr\\/bin\\/docsetutil/docsetutil/" html/Makefile
 cd html && make
+
 cp -R GLFW.docset ../../../
